0111af25c5d17e5d55d9b403e76f51ed3f70bdae
Jack Thacker <jackthacker89@gmail.com>
2022-08-11 16:50:09 +0100

    Added function to generate new game HTML

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 // HOSTILE ALIENS GAME
 
-export const getNewGameHTML = (x, y) => {
-  return x + y;
-}
-
+export const getNewGameHTML = () => {
+  return `
+    <div class="game">
+      <h1 class="game__title">Hostile Aliens</h1>
+      <h4 class="game__subtitle">Shoot to attack the ships</h4>
+      <div class="scores">
+        <h2 class="scores__title"></h2>
+        <div class="scores__container"></div>
+      </div>
+      <div class="ships"></div>
+    </div>
+    `;
+};
--- app.test.js
+++ app.test.js
@@ -4,92 +4,99 @@ game.getNewGameHTML();
 // Preliminary requirements
 
 describe("Hostile Aliens Game", () => {
-
   describe("HTML Function", () => {
     test("Should generate the correct HTML", () => {
-      expect(game.getNewGameHTML(1,2)).toBe(3);
-    })
+      expect(game.getNewGameHTML()).toBe(`
+    <div class="game">
+      <h1 class="game__title">Hostile Aliens</h1>
+      <h4 class="game__subtitle">Shoot to attack the ships</h4>
+      <div class="scores">
+        <h2 class="scores__title"></h2>
+        <div class="scores__container"></div>
+      </div>
+      <div class="ships"></div>
+    </div>
+    `);
+    });
     test("Should target the defined container", () => {
       // expected output
-    })
-  })  
+    });
+  });
 
   xdescribe("Fleet Array", () => {
     // fleet array
     // ship objects
     test("Should store ship data in correct way", () => {
       // expected outcome
-    })
-  })
+    });
+  });
 
   xdescribe("Alien Ship Class", () => {
     // object based on class
     // fleet array
-    test("Should create ship object and assign to fleet array", () => {
-
-    })
+    test("Should create ship object and assign to fleet array", () => {});
 
     test("Ship should have a defined amount of HP", () => {
       // expected outcomes
-    })
+    });
 
     test("Ship should suffer a defined amount of damage", () => {
       // expected outcomes
-    })
+    });
 
     test("Ship should generate correct HTML", () => {
       // expected outcomes
-    })
+    });
 
     test("Ship should get destroyed when HP = 0", () => {
       // expected outcomes (unhittable + no HTML)
-    })
-  })
+    });
+  });
 
   xdescribe("Mother Ship Class", () => {
     test("Should destroy fleet if HP reaches ZERO", () => {
       // expected outcomes
-    })
-  })
+    });
+  });
 
   xdescribe("BTN Class", () => {
     // btn object
     test("Should hit a random ship", () => {
       // expected outcomes
-    })
+    });
 
     test("Should reduce targeted ship's HP on a hit", () => {
       // expected outcomes
-    })
-  })
+    });
+  });
 
   xdescribe("Scores Class", () => {
     // score objects
     test("Should source a score from an array of objects by summing up specific key: value", () => {
       // expected outcomes
-    })
+    });
     test("Should generate HTML for each score", () => {
       // expected outcomes
-    })
-  })
+    });
+  });
 
   xdescribe("Game Over Function", () => {
     // function
     // fleet object
     test("Should load HTML if all ships are destroyed", () => {
       // expected outcomes
-    })
-  })
+    });
+  });
 
   xdescribe("Restart Btn Function", () => {
     // btn function
     // fleet array
     test("Should load correct HTML", () => {
       // expected outcome
-    })
+    });
 
     test("Should restore game data to default", () => {
       // expected outcomes
-    })
-  })
+    });
+  });
 });
