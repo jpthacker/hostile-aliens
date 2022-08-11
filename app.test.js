d3f331a622f4cd1e92b8e428c6bfd1410f647bd6
Jack Thacker <jackthacker89@gmail.com>
2022-08-11 13:25:51 +0100

    Added preliminary test statements and psudocode

---
+++ app.test.js
@@ -0,0 +1,34 @@
+// HOSTILE ALIENS GAME
+
+// Preliminary requirements
+
+describe("Hostile Aliens Game", ( => {
+  describe("HTML Function", () => {
+  
+  })
+}))
+
+// HTML FUNCTION
+// The function should generate the correct HTML
+
+// ALIEN SHIP CLASS
+// Should create an alien ship with a defined amount of hit points
+// The alien ship should suffer a specified amount of damage if hit
+// The creation of each ship should generate HTML
+// I the ship reaches zero hit points it should be destroyed (unhittable + no HTML)
+
+// MOTHER SHIP CLASS
+// Should destroy all other ships if HP reaches 0
+
+// BTN CLASS
+// Should hit a random ship (ships as array?)
+
+// SCORES CLASS
+// Should source a score from an array of objects by summing up specific key: value
+// should generate HTML for each score
+
+// GAME OVER FUNCTION
+// Should load HTML if all ships are destroyed
+
+// BTN FUNCTION
+// Should restart game if clicked
